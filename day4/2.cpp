ed41a831a9870e9dcbed4b2646efda7c71d55d4a
Kanishk Jain <kanishkjain03082005@gmail.com>
2025-12-04 11:41:55 +0530

    day4

---
+++ day4/2.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long ans = 0;
+    vector<string> grid;
+    string filename = "input.txt";
+    ifstream inputFile(filename);
+
+    string line;
+    while (getline(inputFile, line)) { 
+        grid.push_back("."+line+".");
+    }
+    inputFile.close();
+    grid.insert(grid.begin(), string(grid[0].size(), '.'));
+    grid.push_back(string(grid[0].size(), '.'));
+    int m = grid.size(), n = grid[0].size();
+    bool flag = true;
+    while (flag) {
+        flag = false;
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (grid[i][j] == '.') continue;
+                int neigh = (grid[i - 1][j] == '@')
+                            + (grid[i + 1][j] == '@')
+                            + (grid[i][j - 1] == '@')
+                            + (grid[i][j + 1] == '@')
+                            + (grid[i - 1][j - 1] == '@')
+                            + (grid[i - 1][j + 1] == '@')
+                            + (grid[i + 1][j - 1] == '@')
+                            + (grid[i + 1][j + 1] == '@');
+                if (neigh < 4) {
+                    grid[i][j] = '.';
+                    flag = true;
+                    ans++;
+                }
+                            
+            }
+        }
+    }
+    cout << ans << endl;
+
+}
